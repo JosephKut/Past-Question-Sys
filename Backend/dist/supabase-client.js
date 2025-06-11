8cc7afd3aaaf6ddc15ce1162fd4d035730b7ee0e
JosephKut <159574462+JosephKut@users.noreply.github.com>
2025-06-10 17:06:52 -0700

    11th June 2025

---
+++ Backend/dist/supabase-client.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+// console.log(supabaseUrl, supabaseKey);
+export default supabase;
+//# sourceMappingURL=supabase-client.js.map
